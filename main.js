23a3beb16f3479a009c477aeeb2c4dab2e28f7e9
Nikita <nekit.antonoff@gmail.com>
2019-09-25 20:15:16 +0300

    add some effects to car and done lesson 2 whith homework

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 const score = document.querySelector('.score'),
     start = document.querySelector('.start'),
     gameArea = document.querySelector('.gameArea'),
-    car = document.createElement('div');
+    car = document.createElement('div'),
+    fire = document.querySelector('.fire'),
+    smoke = document.querySelector('.smoke'),
+    sound = document.createElement('embed');
 
 car.classList.add('car');
+sound.classList.add('sound');
 
 start.addEventListener('click', startGame);
 document.addEventListener('keydown', startRun);
@@ -28,7 +32,7 @@ const setting = {
 };
 
 function getQuantityElements(heightElement) {
-    return document.documentElement.clientHeight / heightElement + 1;
+    return gameArea.offsetHeight / heightElement + 1;
 }
 
 function startGame() {
@@ -44,16 +48,23 @@ function startGame() {
 
     for (let i = 0; i < getQuantityElements(100 * setting.traffic); i++) {
         const enemy = document.createElement('div');
+        let enemyImg = Math.floor(Math.random() * 3) + 0;
+        console.log('enemyImg: ', enemyImg);
         enemy.classList.add('enemy');
         enemy.y = -100 * setting.traffic * i + 1;
         enemy.style.left = Math.floor(Math.random() * (gameArea.offsetWidth - 50)) + 'px';
         enemy.style.top = enemy.y + 'px';
-        enemy.style.background = 'url(./img/enemy.png)center / contain no-repeat';
+        enemy.style.background = `url(./img/enemy${enemyImg}.png) center / contain no-repeat`;
         gameArea.appendChild(enemy);
     }
 
     setting.start = true;
     gameArea.appendChild(car);
+    sound.setAttribute('type', 'audio/mp3');
+    sound.setAttribute('src', './sound.mp3');
+    gameArea.appendChild(sound);
+    car.appendChild(fire);
+    car.appendChild(smoke);
     setting.x = car.offsetLeft;
     setting.y = car.offsetTop;
     requestAnimationFrame(playGame);
@@ -65,15 +76,19 @@ function playGame() {
         moveEnemy();
         if ((keys.ArrowLeft || keys.a) && setting.x > 0) {
             setting.x -= setting.speed;
+            car.style.transform = 'rotate(-10deg)';
         }
         if ((keys.ArrowRight || keys.d) && setting.x < gameArea.offsetWidth - car.offsetWidth) {
             setting.x += setting.speed;
+            car.style.transform = 'rotate(10deg)';
         }
         if ((keys.ArrowUp || keys.w) && setting.y > 0) {
             setting.y -= setting.speed;
+            fire.style.display = 'block';
         }
         if ((keys.ArrowDown || keys.s) && setting.y < gameArea.offsetHeight - car.offsetHeight) {
             setting.y += setting.speed;
+            smoke.style.display = 'flex';
         }
 
         car.style.left = setting.x + 'px';
@@ -85,13 +100,19 @@ function playGame() {
 
 function startRun(event) {
     event.preventDefault(); // отменяем скроллинг страницы стрелочками
-    keys[event.key] = true; // event - показывает какое событие произошло со всем описанием события. event.key - показыват только нажатую кнопку.
-    console.log(event);
+    if (keys.hasOwnProperty(event.key)) {
+        keys[event.key] = true; // event - показывает какое событие произошло со всем описанием события. event.key - показыват только нажатую кнопку.
+    }
 }
 
 function stopRun(event) {
     event.preventDefault();
-    keys[event.key] = false;
+    car.style.transform = '';
+    fire.style.display = '';
+    smoke.style.display = '';
+    if (keys.hasOwnProperty(event.key)) {
+        keys[event.key] = false; // event - показывает какое событие произошло со всем описанием события. event.key - показыват только нажатую кнопку.
+    }
 }
 
 function moveRoad() {
@@ -100,8 +121,8 @@ function moveRoad() {
         line.y += setting.speed;
         line.style.top = line.y + 'px';
 
-        if (line.y >= document.documentElement.clientHeight) {
-            line.y = -105;
+        if (line.y >= gameArea.offsetHeight) {
+            line.y = -100;
         }
     });
 }
@@ -112,7 +133,7 @@ function moveEnemy() {
         item.y += setting.speed / 2;
         item.style.top = item.y + 'px';
 
-        if (item.y >= document.documentElement.clientHeight) {
+        if (item.y >= gameArea.offsetHeight) {
             item.y = -100 * setting.traffic;
             item.style.left = Math.floor(Math.random() * (gameArea.offsetWidth - 50)) + 'px';
         }
